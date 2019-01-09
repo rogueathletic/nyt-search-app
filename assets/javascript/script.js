6bb62583c7ec41d5379cde47a8bb5b04d7044f65
Darren Victoriano <darren.victoriano@gmail.com>
2019-01-08 21:32:03 -0800

    working we just need to show the result on screen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,46 +1,63 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-
-var apiKey = "19de7cc27a0d4e69bd2724a55ff3ffee",
-    query,
-    beginDate,
-    endDate,
-    page;
-
-var param = {
-    'api-key': apiKey,
-    'q': query,
-    'end_date': beginDate,
-    'begin_date': endDate,
-    'page': page
-};
+var apiKey = "19de7cc27a0d4e69bd2724a55ff3ffee";
 
 function isEmpty(value) {
     return value == null || value == "";
 }
 
-function urlBuilder() {
+function urlBuilder(param) {
     for (key in param)
         if (isEmpty(param[key]))
             delete param[key];
+    console.log(url += '?' + $.param(param))
     return url += '?' + $.param(param);
 }
 
-$('#searchTextBox').on('keypress', function (e) {
+$('#searchTextBar').on('keypress', function (e) {
+
     if (e.which == 13) {
+        // build date
+        var startDate,
+            startMonth,
+            startYear,
+            endDate,
+            endMonth,
+            endYear;
 
-        console.log("asd");
+        // get the data from the textboxes
+        var query = $('#searchTextBar').val(),
+            beginDateFull,
+            endDateFull,
+            page = $('#page').val();
+
+        var param = {
+            'api-key': apiKey,
+            'q': query,
+            'end_date': beginDateFull,
+            'begin_date': endDateFull,
+            'page': page
+        };
+
+        console.log(query);
+        console.log(beginDate);
+        console.log(endDate);
+        console.log(page);
+
+    } else {
+        // API Call
+        $.ajax({
+            url: urlBuilder(param),
+            method: 'GET',
+        }).done(function (result) {
+            console.log(result);
+            console.log(result.response.docs[0].web_url)
+            console.log(result.response.docs[0].snippet)
+        }).fail(function (err) {
+            throw err;
+        });
     }
+
 });
 
-$.ajax({
-    url: urlBuilder(),
-    method: 'GET',
-}).done(function (result) {
-    console.log(result);
-    console.log(testurl);
-    console.log(result.response.docs[0].web_url)
-    console.log(result.response.docs[0].snippet)
-}).fail(function (err) {
-    throw err;
-});
+
 
